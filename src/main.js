79859f8bd3d9ac5c009cecdf5790f02367b23f95
Malachi Maldonado <mmmaldon@ucsc.edu>
2022-05-02 11:23:40 -0700

    Added submission comment

--- src/main.js
+++ src/main.js
@@ -1,3 +1,18 @@
+//========================================================
+// CatSplat
+// By Casey Chen, Eliana Cadelina, and Malachi Maldonado
+// Completed 5/1/22
+// 
+// CatSplat has great visual and audio design thanks to Casey
+// and Eli. We gave the game a very distinct sense of humor
+// and made sure that the gameplay, visuals, and sound all 
+// worked together to enhance that sense of humor.
+//
+// Programming - Casey Chen, Malachi Maldonado
+// Audio - Casey Chen
+// Art - Eliana Cadelina
+// Character/UI Design - Eliana Cadelina 
+//========================================================
 let config = {
     type: Phaser.CANVAS,
     width: 640,
